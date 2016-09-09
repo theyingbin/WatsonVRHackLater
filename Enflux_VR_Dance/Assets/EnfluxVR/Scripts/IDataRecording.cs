5aba4b9829afa81586d3673a67ec99807c34decd
Ying Bin Wu <ying@enfluxmobile.com>
2016-09-09 15:55:29 -0700

    Revert to previous commit

--- Enflux_VR_Dance/Assets/EnfluxVR/Scripts/IDataRecording.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IDataRecording {
-    void addUpper(float[] upper);
-    void addLower(float[] lower);
-    void writeData();
-    bool shouldRecord();
-}
